d6696357f61770c7ca99a0e4ae28c962e4b9307b
almostdevelopr <minhalam.dev@gmail.com>
2022-06-06 18:55:34 +0530

    add list operations

---
+++ python_work/Lists/list_ops.py
@@ -0,0 +1,28 @@
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles)
+
+# add element at the end of the list
+motorcycles.append("ducati")
+print(motorcycles)
+
+cars = []
+cars.append("honda")
+cars.append("maruti")
+cars.append("audi")
+print(cars)
+
+# inserting element into a list -> insert(index,element)
+cars.insert(1, "bmw")
+print(cars)
+
+# deleting form the list at given index
+del cars[1]
+del cars[2]
+print(cars)
+
+
+# pop() -> delete item from list and use after deleting
+print("****** POP() ******")
+print(motorcycles)
+popped_motorcycle = motorcycles.pop()
+print(popped_motorcycle)
