a3f97a3f73a6318b49f02671f8315b57312c0674
almostdevelopr <minhalam.dev@gmail.com>
2022-06-06 16:39:52 +0530

    add stripping whitespaces

---
+++ python_work/favourite_language.py
@@ -0,0 +1,9 @@
+favourite_language = "  python "
+# remove whitespace from left side
+print(favourite_language.lstrip())
+
+# remove whitespace from right side
+print(favourite_language.rstrip())
+
+# remove whitespace from both the sides
+print(favourite_language.strip())
--- python_work/full_name.py
+++ python_work/full_name.py
@@ -1,11 +1,13 @@
 first_name = "ada"
 last_name = "lovelace"
-full_name=f"{first_name} {last_name}" #format string -> f-strings
+full_name = f"{first_name} {last_name}"  # format string -> f-strings
 
-# To insert a variable’s value into a string, place the letter f 
-# immediately before the opening quotation mark. Put braces around 
-# the name or names of any variable you want to use inside the string. 
+# To insert a variable’s value into a string, place the letter f
+# immediately before the opening quotation mark. Put braces around
+# the name or names of any variable you want to use inside the string.
 # Python will replace each variable with its value when the string is displayed.
-# These strings are called f-strings. The f is for format, because Python 
+# These strings are called f-strings. The f is for format, because Python
 # formats the string by replacing the name of any variable in braces with its value.
-print(full_name.title())
+# print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
