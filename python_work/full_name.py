1007fcd72bd8ac97df1e16f9ac47cecb2b20ba4e
almostdevelopr <minhalam.dev@gmail.com>
2022-06-06 16:09:33 +0530

    string variables

---
+++ python_work/full_name.py
@@ -0,0 +1,11 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name=f"{first_name} {last_name}" #format string -> f-strings
+
+# To insert a variable’s value into a string, place the letter f 
+# immediately before the opening quotation mark. Put braces around 
+# the name or names of any variable you want to use inside the string. 
+# Python will replace each variable with its value when the string is displayed.
+# These strings are called f-strings. The f is for format, because Python 
+# formats the string by replacing the name of any variable in braces with its value.
+print(full_name.title())
--- python_work/hello.py
+++ python_work/hello.py
@@ -1,5 +1,12 @@
 message="Hello Python World!"
-print(message)
+print(message.upper())
+print(message.lower())
 
-mesage = "Hello Python Crash Course world!"
-print(mesage)
+mesage = "hello PYTHON Crash Course world!"
+print(mesage.title())
+
+# The lower() method is particularly useful for storing data. 
+# Many times you won’t want to trust the capitalization that your 
+# users provide, so you’ll convert strings to lowercase before storing them. 
+# Then when you want to display the information, you’ll use the case that makes
+#  the most sense for each string.
