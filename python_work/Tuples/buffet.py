ea269a6dab19a90897af1979f15481c04cc2b66a
almostdevelopr <minhalam.dev@gmail.com>
2022-06-07 17:26:39 +0530

    lists ops

---
+++ python_work/Tuples/buffet.py
@@ -0,0 +1,12 @@
+basic_foods = ("rice", "omlt", "pizza", "burger", "bread")
+
+# basic_foods[2] = "bla bla"
+
+print("Previous menu:")
+for food in basic_foods:
+    print(f"Restaurant offers {food}")
+
+revised_menu = ("rice", "omlt", "milk", "burger", "bread")
+print("Revised menu:")
+for food in revised_menu:
+    print(food)
---
+++ python_work/Tuples/dimensions.py
@@ -0,0 +1,15 @@
+dimensions = (200, 15)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 54)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
+print(dimensions[0])
+print(dimensions[1])
+
+# dimensions[1] = 23
+my_t = (3,)
+print(type(my_t))
