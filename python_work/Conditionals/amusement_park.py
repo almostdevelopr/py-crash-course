c3b50d045bfd411e45ae781261d5cc7dfde17c43
almostdevelopr <minhalam.dev@gmail.com>
2022-06-08 12:58:00 +0530

    add if-elif-else

--- python_work/Conditionals/amusement_park.py
+++ python_work/Conditionals/amusement_park.py
@@ -1,7 +1,8 @@
 age = 17
 if age < 4:
-    print("Your admission cost $0.")
+    price = 0
 elif age < 18:
-    print("Your admission cost is $25.")
+    price = 25
 else:
-    print("Your admission cost is $40.")
+    price = 40
+print(f"Your admission cost is ${price}")
---
+++ python_work/Conditionals/favorite_fruits.py
@@ -0,0 +1,11 @@
+favorite_fruits = ["apple", "orange", "grapes", "mango", "watermellon"]
+if "apple" in favorite_fruits:
+    print("You really like apple!")
+if "banana" not in favorite_fruits:
+    print("You don't like banana?")
+if "orange" in favorite_fruits:
+    print("You really like orange!")
+if "grapes" in favorite_fruits:
+    print("You really like grapes!")
+if "mango" in favorite_fruits:
+    print("You really like mango!")
