124eaddf2e6438bb72b42d1b527068d39032d447
almostdevelopr <minhalam.dev@gmail.com>
2022-06-08 16:13:32 +0530

    add dictionary examples

--- python_work/Dictionary/favorite_languages.py
+++ python_work/Dictionary/favorite_languages.py
@@ -11,3 +11,18 @@ print(favorite_language)
 
 for name, language in favorite_languages.items():
     print(f"{name.title()}'s favorite language is {language.title()}")
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+print("")
+
+friends = ["sarah", "phil"]
+for name, language in favorite_languages.items():
+    print(f"Hi {name.title()}")
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"{name.title()}, I see you love {language.title()}!")
+
+if "erin" not in favorite_languages.keys():
+    print("Erin, please take our poll!")
