8d1c6d63ec4320f08a15a941bb23298271f5e95e
almostdevelopr <minhalam.dev@gmail.com>
2022-06-08 22:07:13 +0530

    import function from module

--- python_work/Functions/making_pizza.py
+++ python_work/Functions/making_pizza.py
@@ -1,5 +1,9 @@
 import pizza
 
 # module_name.function_name()
+# importing function from module file
+# from module_name import function_name_0,function_name_1,function_name_2
+# from pizza import make_pizza
+# make_pizza(args,arggss)
 pizza.make_pizza(12, "mushrooms")
 pizza.make_pizza(32, "mushrooms", "green peppers", "extra cheese")
