6fd2cae03fd8c05ccec357c6c4bae81bf9d27133
almostdevelopr <minhalam.dev@gmail.com>
2022-06-08 22:04:08 +0530

    add function and module

--- python_work/Functions/greeter.py
+++ python_work/Functions/greeter.py
@@ -14,3 +14,28 @@ def get_formatted_name(first_name, last_name):
 
 musician = get_formatted_name("jimi", "hendrix")
 print(musician)
+
+# models printing for 3D company
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, untill none are left.
+    Move each design to completed models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing models: {current_design}")
+        completed_models.append(current_design)
+
+
+def show_completed_models(completed_models):
+    """Sow all the models that were printed."""
+    print("\nThe following models have been printed.")
+    for completed_model in completed_models:
+        print(completed_model)
+
+
+unprinted_designs = ["phone case", "robot pendant", "dodecahedron"]
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
---
+++ python_work/Functions/making_pizza.py
@@ -0,0 +1,5 @@
+import pizza
+
+# module_name.function_name()
+pizza.make_pizza(12, "mushrooms")
+pizza.make_pizza(32, "mushrooms", "green peppers", "extra cheese")
