331e892b115997b2ba5ea0574adefa21f31101a4
almostdevelopr <minhalam.dev@gmail.com>
2022-06-08 20:58:10 +0530

    Add while and user inouts

---
+++ python_work/Inputs/restaurant_seating.py
@@ -0,0 +1,7 @@
+number_of_person = input("Please enter number of person in you dinner group: ")
+number_of_person = int(number_of_person)
+
+if number_of_person > 8:
+    print("Guys, you'll have to wait for the table.")
+else:
+    print("Your table is ready!")
