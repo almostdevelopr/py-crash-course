c8ebb5ee007650109aee0d8909c5b74701727793
almostdevelopr <minhalam.dev@gmail.com>
2022-06-10 14:15:08 +0530

    Read and write in a file

--- python_work/Files Exceptions/file_reader.py
+++ python_work/Files Exceptions/file_reader.py
@@ -1,4 +1,6 @@
 with open("pi_digits.txt") as file_object:
-    contents = file_object.read()
+    # contents = file_object.read()
+    for line in file_object:
+        print(line.rstrip())
 
-print(contents.strip())
+# print(contents.strip())
---
+++ python_work/Files Exceptions/write_message.py
@@ -0,0 +1,8 @@
+filename_0 = "programming.txt"
+
+with open(filename_0, "w") as file_object:
+    file_object.write("I love Python.")
+
+filename_1 = "python.txt"
+with open(filename_1, "w") as file_object:
+    file_object.write("We can use Python language in almost all the fields.")
